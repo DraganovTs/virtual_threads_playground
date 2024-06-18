bdedc0e4cfabf3873c3cb74217048fd1435a5b2c
DraganovTs <cvetomir_draganov@abv.bg>
2024-06-18 20:34:30 +0300

    virtual thread

--- src/main/java/com/home/exam/sec01/InboundOutboundTaskDemo.java
+++ src/main/java/com/home/exam/sec01/InboundOutboundTaskDemo.java
@@ -5,10 +5,11 @@ import java.util.concurrent.CountDownLatch;
 public class InboundOutboundTaskDemo {
 
     private static final int MAX_PLATFORM = 10;
+    private static final int MAX_VIRTUAL = 20;
 
 
     public static void main(String[] args) throws InterruptedException {
-        platformThreadDemo3();
+        virtualThreadDemo();
     }
 
     private static void platformThreadDemo1() {
@@ -45,6 +46,24 @@ public class InboundOutboundTaskDemo {
             });
             thread.start();
         }
+        latch.await();
+    }
+
+
+    private static void virtualThreadDemo() throws InterruptedException {
+        var latch = new CountDownLatch(MAX_VIRTUAL);
+
+        var builder = Thread.ofVirtual().name("virtual", 1);
+
+        for (int i = 0; i < MAX_VIRTUAL; i++) {
+            int j = i;
+            Thread thread = builder.unstarted(() -> {
+                Task.ioIntensive(j);
+                latch.countDown();
+            });
+            thread.start();
+        }
+
         latch.await();
     }
 }
--- src/main/java/com/home/exam/sec01/Task.java
+++ src/main/java/com/home/exam/sec01/Task.java
@@ -12,9 +12,9 @@ public class Task {
 
     public static void  ioIntensive(int i){
         try {
-            log.info("starting I/O task {}",i);
+            log.info("starting I/O task {}, Thread info {}",i,Thread.currentThread());
             Thread.sleep(Duration.ofSeconds(10).toSeconds());
-            log.info("ending I/O task {}",i);
+            log.info("ending I/O task {}, Thread info {}",i,Thread.currentThread());
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
